c92e506acf9d513bcbd26988ecd5d3413ee9b267
Eliseo Ferrante <eferrant@ulb.ac.be>
2014-04-17 20:07:43 +0200

    Test second version

--- src/apgof2/Apgof.java
+++ src/apgof2/Apgof.java
@@ -4,6 +4,10 @@ import io.CLIParser;
 import io.Configuration;
 import io.ParameterXMLParser;
 
+/** APGoF 2 - The best simulator ever
+ * @author eferrante
+ *
+ */
 public class Apgof {
 
 	public static void main(String[] args) {
